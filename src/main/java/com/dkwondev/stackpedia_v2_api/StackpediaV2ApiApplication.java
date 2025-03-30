ad2186aa26fdb80ac656364b9bff4c8f3aec0528
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2025-03-29 18:28:11 -0600

    Initial commit

---
+++ src/main/java/com/dkwondev/stackpedia_v2_api/StackpediaV2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.dkwondev.stackpedia_v2_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackpediaV2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackpediaV2ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dkwondev/stackpedia_v2_api/StackpediaV2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkwondev.stackpedia_v2_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackpediaV2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
