902d0eae3cbfcc26264ae9c366bbd686c05df649
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2025-03-29 18:49:37 -0600

    Create User entity

---
+++ src/main/java/com/dkwondev/stackpedia_v2_api/model/entity/User.java
@@ -0,0 +1,34 @@
+package com.dkwondev.stackpedia_v2_api.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @NotEmpty(message = "username cannot be empty")
+    @Column(name = "username")
+    private String username;
+
+    @NonNull
+    @NotEmpty(message = "email cannot be empty")
+    @Column(name = "email")
+    private String email;
+
+    @NonNull
+    @NotEmpty(message = "password cannot be empty")
+    @Column(name = "password")
+    private String password;
+
+}
