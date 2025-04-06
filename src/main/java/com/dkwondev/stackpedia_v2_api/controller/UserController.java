2e70bd5d4bc7600cd4637e61bba4e2e3e99fbd0b
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2025-04-05 19:34:43 -0600

    Add basic security filter chain

--- src/main/java/com/dkwondev/stackpedia_v2_api/controller/UserController.java
+++ src/main/java/com/dkwondev/stackpedia_v2_api/controller/UserController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class UserController {
 
-    private UserService userService;
-    private UserMapper userMapper;
+    private final UserService userService;
+    private final UserMapper userMapper;
 
     @PostMapping("/signup")
     public ResponseEntity<UserDTO> signup(@RequestBody User user) {
