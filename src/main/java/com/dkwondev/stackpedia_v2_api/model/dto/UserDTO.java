de7ade291df45a83cb3a6b54daef9fe7317f6c2a
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2025-04-05 19:53:44 -0600

    Refactor user entity to work with roles

--- src/main/java/com/dkwondev/stackpedia_v2_api/model/dto/UserDTO.java
+++ src/main/java/com/dkwondev/stackpedia_v2_api/model/dto/UserDTO.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDTO {
-    private Long id;
+    private Long userId;
     private String username;
     private String email;
 }
--- src/main/java/com/dkwondev/stackpedia_v2_api/model/entity/User.java
+++ src/main/java/com/dkwondev/stackpedia_v2_api/model/entity/User.java
@@ -3,6 +3,12 @@ package com.dkwondev.stackpedia_v2_api.model.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -12,11 +18,12 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
 
     @NonNull
     @NotEmpty(message = "username cannot be empty")
@@ -33,4 +40,36 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = {@JoinColumn(name = "user_id")},
+        inverseJoinColumns = {@JoinColumn(name = "role_id")}
+    )
+    private Set<Role> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
