7a86f70538783c3701434bcb0342ed6cccc836eb
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2025-06-28 21:53:34 -0600

    Change all technologies response to sort alphabetically

--- src/main/java/com/dkwondev/stackpedia_v2_api/repository/TechnologyRepository.java
+++ src/main/java/com/dkwondev/stackpedia_v2_api/repository/TechnologyRepository.java
@@ -4,7 +4,11 @@ import com.dkwondev.stackpedia_v2_api.model.entity.Technology;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TechnologyRepository extends JpaRepository<Technology, Long> {
     Technology findBySlug(String slug);
+
+    List<Technology> findAllByOrderByNameAsc();
 }
