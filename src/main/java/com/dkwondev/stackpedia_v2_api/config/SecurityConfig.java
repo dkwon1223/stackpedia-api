c09a6cfc3b9e21ba98f9f2c410d7ef68d5ccbd3d
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2025-06-30 21:37:50 -0600

    Modify CORS config for deployed origin

--- src/main/java/com/dkwondev/stackpedia_v2_api/config/SecurityConfig.java
+++ src/main/java/com/dkwondev/stackpedia_v2_api/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://www.stackpedia.dev", "https://stackpedia.dev"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
